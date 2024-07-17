ef03ec1647e5f69f4ce7aa234a951f6580b248f0
Nathalia <nathalia96234@gmail.com>
2024-07-17 21:29:21 +0100

    let

--- script.js
+++ script.js
@@ -44,3 +44,33 @@ console.log(operate('+', 42,2));
 console.log(operate('-', 60, 5));
 console.log(operate('*', 7, 11));
 console.log(operate('/', 176, 2));
+
+let currentInput: '';
+let previousInput: '';
+let currentOperator= null;
+let shouldResetScreen = false;
+
+const display= document.querySelector('.display');
+
+const clear = () => {
+        currentInput = '';
+        currentOperator = null;
+        shouldResetScreen = false;
+        updateDisplay();
+};
+
+const addingNumber = (number) => {
+    if (shouldResetScreen) {
+        currentInput = '';
+        shouldResetScreen = false;
+    }
+    currentInput += number;
+    updateDisplay();
+};
+
+const chooseOperator = (operator) => {
+    if (currentOperator !== null) calculate();
+    previousInput = currentInput;
+    currentOperator = operator;
+    shouldResetScreen = true;
+};
